d5ee27bd914994956fe7fe9d1dcaf10d94558812
ceceliacreates <40367173+ceceliacreates@users.noreply.github.com>
2019-11-04 19:54:29 -0500

    initial commit of v1 functionality

--- src/App.js
+++ src/App.js
@@ -1,26 +1,115 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Fragment } from 'react';
 import './App.css';
+import { Typography, Card, Grid, CardContent } from '@material-ui/core';
+import bingoItems from './bingoItems.json';
+import { styled } from '@material-ui/core/styles';
 
-function App() {
+const BingoItem = styled(Card)({
+  textAlign: 'center',
+  background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+  color: 'white'
+})
+
+const CompletedItem = styled(Card)({
+  textAlign: 'center',
+  background: 'linear-gradient(45deg, #1e824c 30%, #4ecdc4 90%)',
+  color: 'white'
+})
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      0: false,
+      1: false,
+      2: false,
+      3: false,
+      4: false,
+      5: false,
+      6: false,
+      7: false,
+      8: false,
+      9: false,
+      10: false,
+      11: false
+    };
+    this.markComplete = this.markComplete.bind(this);
+  }
+
+  bingoRow(row) {
+    let rowOne = bingoItems.slice(0, 3);
+    let rowTwo = bingoItems.slice(3, 6);
+    let rowThree = bingoItems.slice(6, 9);
+    let rowFour = bingoItems.slice(9, 12);
+    let renderRow;
+    switch (row) {
+      case 1:
+        renderRow = rowOne;
+        break;
+      case 2:
+        renderRow = rowTwo;
+        break;
+      case 3:
+        renderRow = rowThree;
+        break;
+      case 4:
+        renderRow = rowFour;
+        break;
+      }
+    return (
+    <Grid container justify="center" alignItems="center" alignContent="center" spacing={3}>
+      {renderRow.map(item => {
+        const id = item.id;
+        if (this.state[id] === true) {
+          return (
+            <Grid item xs={3} key={item.id}>
+              <CompletedItem id={item.id}>
+                <CardContent>
+                <Typography variant='body' gutterBottom>Complete</Typography>
+                </CardContent>
+              </CompletedItem>
+              </Grid>
+          )
+        }
+        else {
+          return (
+           <Grid item xs={3} key={item.id}>
+          <BingoItem id={item.id} onClick={this.markComplete}> 
+          <CardContent>
+            <Typography variant='body' id={item.id} onClick={this.markComplete} gutterBottom>
+            {item.item}</Typography>
+          </CardContent>
+          </BingoItem>
+        </Grid> 
+          )
+        }
+      })
+      }
+      </Grid>
+    )
+  }
+
+  markComplete(e) {
+    const id = e.target.id;
+    this.setState({[id]: true})
+
+  }
+
+  render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Grid container justify="center" alignItems="center">
+        <Typography variant='h3' gutterBottom>
+          Icebreaker Bingo
+      </Typography>
+      </Grid>
+      {this.bingoRow(1)}
+      {this.bingoRow(2)}
+      {this.bingoRow(3)}
+      {this.bingoRow(4)}
+    </>
+  )};
 }
 
 export default App;
